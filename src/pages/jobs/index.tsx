8f5b042a0de425ebe418587ed159f30c88446175
jayasuryaa-govindraj <jayasuryaa.govindraj1998@gmail.com>
2023-07-07 11:03:13 -0400

    Added yesbuffalo logo.

--- src/pages/jobs/index.tsx
+++ src/pages/jobs/index.tsx
@@ -7,25 +7,29 @@ import { useEffect, useState } from "react";
 export default function Jobs() {
   const [jobs, setJobs] = useState<CompanyInfo[]>([]);
   useEffect(() => {
-    fetch("temp.json", {
+    fetch(API + "get_companies", {
       headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
       },
     })
-    .then((response) => response.json())
-    .then((data) => setJobs(data));
+      .then((response) => response.json())
+      .then((data) => setJobs(data));
   }, []);
   return (
-    <div style={{padding: 10}}>
-      <Typography variant="h3" sx={{ color: "purple", mx: 3, my: 1 }}>Say Yes Buffalo</Typography>
-      <Typography variant="h4" sx={{marginLeft: 3, my: 2}}>Job Portal</Typography>
+    <div style={{ padding: 10 }}>
+      <div style={{ display: "flex", alignContent: "center", alignItems:"center" }}>
+        <img src="SayYesBuffalo.jpeg" alt="Say Yes Buffalo Logo" style={{ width: "25%", maxWidth: 500, height: "auto" }} />
+        <Typography variant="h4" sx={{ marginLeft: 3, my: 2 }}>
+          Job Portal
+        </Typography>
+      </div>
 
       <Grid container spacing={2} display="flex" justifyContent="flex-start">
         {jobs.map((job) => (
           <Grid item xs={12} sm={6} md={4} key={job.job_id}>
-          <CompanyCard job_id={job.job_id} title={job.title} company={job.company} location={job.location} tools={job.tools} link={job.link} />
-        </Grid>
+            <CompanyCard job_id={job.job_id} title={job.title} company={job.company} location={job.location} tools={job.tools} link={job.link} />
+          </Grid>
         ))}
       </Grid>
     </div>
